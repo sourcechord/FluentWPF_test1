6a5e0ee5980e15447351bed5164b89f3905a8d81
sourcechord <sourcechord@gmail.com>
2019-07-24 00:40:23 +0900

    Add projects

---
+++ FluentWPFTest/FluentWPFTest/Application.cs
@@ -0,0 +1,24 @@
+﻿using SourceChord.FluentWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FluentWPFTest
+{
+    public class Application
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            System.Windows.Application app = new System.Windows.Application();
+            app.Run(new MainWindow());
+        }
+    }
+
+    public class MainWindow : AcrylicWindow
+    {
+    }
+}
